c372d218c7553064b26d42d0f287bef32d972974
rasindus <160561869+rasindus@users.noreply.github.com>
2024-06-27 07:43:52 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94726785433' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=yNsDhKRL#EJ7SE17hwu4mblBz0toeevsxUs9xyjTFP-gMkInFilE' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=mNsijAyI#EG_MpKr88iHPLJ9MRGSQiUQ41VeclNtKvJcteJKDgFQ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
